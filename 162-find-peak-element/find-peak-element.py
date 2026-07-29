368cf8a1ba3fc2a33572436c29aaf8a51a29dedc
pushkarsambyal-a11y <pushkarsambyal@gmail.com>
2026-07-29 11:28:39 +0530

    Update find-peak-element.py

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,10 +1,15 @@
 class Solution(object):
     def findPeakElement(self, nums):
-        for i in range(len(nums) - 1):
-            if nums[i] > nums[i + 1] and nums[i] > nums[i - 1]:
-                return i
-        
-        return len(nums) - 1
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        return left
 
              
         
