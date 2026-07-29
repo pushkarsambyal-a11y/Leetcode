9fe61cc708b60473a7861411399da2e734aaa599
pushkarsambyal-a11y <pushkarsambyal@gmail.com>
2026-07-29 11:20:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (52.47%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def findPeakElement(self, nums):
         for i in range(len(nums) - 1):
-            if nums[i] > nums[i + 1]:
+            if nums[i] > nums[i + 1] and nums[i] > nums[i - 1]:
                 return i
         
         return len(nums) - 1
